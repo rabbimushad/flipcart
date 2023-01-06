2a74b85712e586092029c0f2485738c2d6b77561
mushad <dhushordhrubo@gmail.com>
2023-01-06 16:16:42 +0000

    full project

---
+++ backend/config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
+dotenv.config();
+
+const MONGO_URI = process.env.MONGO_URI;
+const connectDatabase = () => {
+    mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => {
+            console.log("Mongoose Connected");
+        });
+}
+
+module.exports = connectDatabase;
